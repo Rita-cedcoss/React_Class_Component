d1e0531e9600d3682b57fb82f771ee09607979fe
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-26 11:58:08 +0530

    hello world task completed in react

---
+++ src/component/HelloWorld.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div className='hello'>Hello World!</div>
+    )
+  }
+}
+export default HelloWorld
